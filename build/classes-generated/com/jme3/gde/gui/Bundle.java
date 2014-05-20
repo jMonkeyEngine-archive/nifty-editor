b61915e0acc490821cf99aca44ada2c43ed67583
relu91 <relu.cri@gmail.com>
2014-05-20 10:19:20 +0200

    Init repository

---
+++ build/classes-generated/com/jme3/gde/gui/Bundle.java
@@ -0,0 +1,27 @@
+package com.jme3.gde.gui;
+/** Localizable strings for {@link com.jme3.gde.gui}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>XML</i>
+     * @see NiftyGuiDataObject
+     */
+    static String LBL_NiftyGui_EDITOR() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_NiftyGui_EDITOR");
+    }
+    /**
+     * @return <i>Files of NiftyGui</i>
+     * @see NiftyGuiDataObject
+     */
+    static String LBL_NiftyGui_LOADER() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_NiftyGui_LOADER");
+    }
+    /**
+     * @return <i>Visual</i>
+     * @see NiftyGuiVisualElement
+     */
+    static String LBL_NiftyGui_VISUAL() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_NiftyGui_VISUAL");
+    }
+    private void Bundle() {}
+}
---
+++ build/classes-generated/com/jme3/gde/gui/swing/Bundle.java
@@ -0,0 +1,13 @@
+package com.jme3.gde.gui.swing;
+/** Localizable strings for {@link com.jme3.gde.gui.swing}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Convert Project Swing classes to NiftyGUI</i>
+     * @see ConvertToNifty
+     */
+    static String CTL_ConvertToNifty() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_ConvertToNifty");
+    }
+    private void Bundle() {}
+}
